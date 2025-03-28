45d3a791162981965006b2b7f5bcc4865aaba055
Nnenna Okoye <nnennaokoye001@gmail.com>
2025-03-29 00:59:58 +0100

    commit

--- src/Bitwise.sol
+++ src/Bitwise.sol
@@ -4,22 +4,22 @@ pragma solidity ^0.8.28;
 contract Bitwise {
     uint256 public packed;
 
-    function addValue(uint256 value, uint8 loc) public {
-        require(loc < 32, "Slot must be less than 32");
-        uint shift = uint256(loc) * 8;
+    function addValue(uint256 value, uint8 slot) public {
+        require(slot < 32, "Slot must be less than 32");
+        uint shift = uint256(slot) * 8;
         packed = packed | (uint256(value )<< shift);
     }
 
-    function getValue(uint8 loc) public view returns (uint8) {
-        require(loc < 32, "Slot must be less than 32");
-        uint256 shift = uint256(loc) * 8;
+    function getValue(uint8 slot) public view returns (uint8) {
+        require(slot < 32, "Slot must be less than 32");
+        uint256 shift = uint256(slot) * 8;
         return uint8((packed >> shift) & 0xff);
     }
 
     function getAllValues() public view returns (uint8[] memory) {
         uint8[] memory values = new uint8[](32);
-        for (uint8 loc = 0; loc < 32; loc++) {
-            values[loc] = getValue(loc);
+        for (uint8 slot = 0; slot < 32; slot++) {
+            values[slot] = getValue(slot);
         }
         return values;
     }
